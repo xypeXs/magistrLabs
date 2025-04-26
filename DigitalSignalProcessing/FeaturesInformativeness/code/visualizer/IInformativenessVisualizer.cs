fa424de6d2aaab1a0efc0b1278fb0696ccd73739
xypeXs <xypexs173@gmail.com>
2025-04-26 17:09:07 +0300

    Added informativeness visualizer

---
+++ DigitalSignalProcessing/FeaturesInformativeness/code/visualizer/IInformativenessVisualizer.cs
@@ -0,0 +1,14 @@
+﻿using code.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visualizer
+{
+    public interface IInformativenessVisualizer
+    {
+        public void visualize(InformativenessCalculationResult informativeness);
+    }
+}
